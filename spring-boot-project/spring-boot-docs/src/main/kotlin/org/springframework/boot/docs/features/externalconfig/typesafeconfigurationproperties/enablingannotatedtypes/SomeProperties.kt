ebdfaea1cfece0019ef01562b9b743d0c71494dc
Linzherong <78370412@qq.com>
2026-01-11 23:53:19 +0800

    init

---
+++ spring-boot-project/spring-boot-docs/src/main/kotlin/org/springframework/boot/docs/features/externalconfig/typesafeconfigurationproperties/enablingannotatedtypes/SomeProperties.kt
@@ -0,0 +1,7 @@
+package org.springframework.boot.docs.features.externalconfig.typesafeconfigurationproperties.enablingannotatedtypes
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("some.properties")
+class SomeProperties
+
